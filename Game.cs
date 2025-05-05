d7a83a998998bd43f1b036d696949b92bdd360b7
YannickLeCam <149806445+YannickLeCam@users.noreply.github.com>
2025-05-05 11:27:07 +0200

    Ajoutez des fichiers projet.

---
+++ Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class Game
+    {
+        //If 0 = vide if 1 = snake if 2 = fruit
+        private int[,] _grille;
+        private int _largeurGrille;
+        private int _longueurGrille;
+        private int _nbTour = 0;
+        private Serpent _serpent;
+        private char _keyPressed;
+
+        public Game(int largeurGrille , int longueurGrille) 
+        {
+            _longueurGrille = longueurGrille;
+            _largeurGrille = largeurGrille;
+            _grille = new int[largeurGrille,longueurGrille];
+            _serpent = new Serpent(largeurGrille,longueurGrille);
+            //Set up du serpent de base
+            _grille[largeurGrille/2,longueurGrille/2] = 1;
+            Random rdm = new Random();
+            //On set up un fruit
+            _grille[rdm.Next(0, largeurGrille), rdm.Next(0, longueurGrille)] = 2;
+        }
+
+        public void affiGrille()
+        {
+
+        }
+
+    }
+}
---
+++ Serpent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class Serpent
+    {
+        private int _taille = 1;
+        private Queue<Point> _coordonnee = new Queue<Point>();
+
+        public Serpent(int grilleLargeur , int grillerLongueur) 
+        {
+            this._coordonnee.Enqueue(new Point((int)grilleLargeur/2, (int)grillerLongueur/2));
+        }
+
+        public bool seDeplacer(char keyPress , List<List<int>> Grille)
+        {
+
+        }
+
+    }
+}
